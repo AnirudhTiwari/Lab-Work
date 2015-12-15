97ca2f591ae82cba5c45345a7450552e6bb0a3ca
Anirudh Tiwari <tiwariisonline@gmail.com>
2015-12-15 22:31:11 +0530

    Final work for length, rg and interaction

--- Scripts/calculateInteractionEnergy.py
+++ Scripts/calculateInteractionEnergy.py
@@ -340,26 +340,37 @@ def fillVoids(boundaries):
 	return boundaries
 
 # path = "../Output Data/Two Domain Proteins/"
-path = "../Output Data/500_proteins/Contiguous/"
 
 file_counter = 0
 correct = 0
 accuracy = 0.0
-length_list = []
-interactionEnergy_list = []
-length_list_2 = []
-interactionEnergy_list_2 = []
+length_list_a = []
+interactionEnergy_list_a = []
+length_list_b = []
+interactionEnergy_list_b = []
 visited_list = []
 
-gyration_list = []
-gyration_list_2 = []
+gyration_list_a = []
+gyration_list_b = []
 
+a = int(raw_input("No. of domains(lesser) for protein A: "))
+b = int(raw_input("No. of domains(higher) for protein B: "))
+c = raw_input("Contiguous(C)/Non-Contiguous(N)/Both(B): ")
+
+# if c=='C':
+# 	path = "../Output Data/500_proteins/Contiguous/"
+
+# if c=='N':
+# 	path = "../Output Data/500_proteins/Non Contiguous/"
+
+path = "../Output Data/500_proteins/Both/"
+
+
+
+# print a, b, c
 
 # print "No., PDB, Domains, CATH, Interaction Energy for 1 Domain, Interaction Energy for 2 Domains, Interaction Energy for 3 Domains, Interaction Energy for 4 Domains, Interaction Energy for 5 Domain, Interaction Energy for 6 Domain"
 for pdb_file in os.listdir(path):
-
-	# if len(length_list) >= 15 and len(length_list_2) >= 15:
-	# 	break
 
 	pdb_path = pdb_file
 	pdb_file = pdb_file.split(".")[0].lower()
@@ -368,7 +379,7 @@ for pdb_file in os.listdir(path):
 
 
 	var = open('../Input Files/CathDomall', 'r')
-	not_list = ['3g4s', '1adh', '1baa', '1a4k', '1abk','2w8p']
+	not_list = ['3g4s', '1adh', '1baa', '1a4k', '1abk','2w8p', '1tj7', '1z5h']
 
 
 	while 1:
@@ -379,83 +390,55 @@ for pdb_file in os.listdir(path):
 			break
 
 		else:
-
 			if pdb_id[:4].lower()==pdb_file[:4].lower()  and pdb_file not in not_list and pdb_id[:4] not in visited_list:
 				flag = 1
-
 				var_1 = open(path+pdb_path, 'r')
-
 				frags = int(pdb_id[11] + pdb_id[12])
-				# print "frags ", frags
 				var_2 = open(path+pdb_path, 'r')
-
-				# var_2 = var_1
-				# print pdb_id[:4]
-
 				chain = pdb_id[18]
-				
-
-
 				domains = int(pdb_id[7] + pdb_id[8])
 
-
-				# print pdb_id[:4], domains
-
-				if frags==0 and domains==1 and pdb_id[:4]:
+				if frags==0 and domains==a and pdb_id[:4]:
 					domain_boundary = pdb_id[14:].strip()
 
 					visited_list.append(pdb_id[:4])
 
-					# print str(file_counter + 1) + "," + pdb_id[:4] + ", " + str(domains) + ", " + domain_boundary, "," ,
 					cords_list, realId_list = getCordsList(var_1,chain)
 
 					x = np.asarray(cords_list)
-					# print str(file_counter + 1) + ", " + pdb_id[:4] + ", "+ str(domains) + ", " + domain_boundary, "," ,
 
 					file_counter+=1
 
-
 					simulated_domains = 1
 
-					while(simulated_domains!=3):
-
+					while(simulated_domains!=b+1):
 
 						km = KMeans(n_clusters=simulated_domains).fit(x)
-
 						labels_km = km.labels_
 
-						# print "Simulated Domains = " + str(simulated_domains),
-
-						if simulated_domains==2:
-							length_list.append(len(cords_list))
-							gyration_list.append(radius_of_gyration.radius_of_gyration(var_2, chain))
+						if simulated_domains==b:
+							length_list_a.append(len(cords_list))
+							gyration_list_a.append(radius_of_gyration.radius_of_gyration(var_2, chain))
 							interaction_energy = getInteractionEnergy(labels_km, simulated_domains, cords_list)
-							interactionEnergy_list.append(interaction_energy)
-							# print pdb_id[:4], chain, len(cords_list), interaction_energy
-
-						# if simulated_domains==6:
-						# 	print "{0:.3f}".format(getInteractionEnergy(labels_km, simulated_domains, cords_list))
-						# else:
-						# 	print "{0:.3f}".format(getInteractionEnergy(labels_km, simulated_domains, cords_list)), ", ",
-
+							interactionEnergy_list_a.append(interaction_energy)
 						simulated_domains+=1
 
-				# bndries =  pdb_id[14:].strip()
 
-
-
-				if frags==0 and domains==2 and pdb_id[:4]:
-					# print pdb_id
+				if frags==0 and domains==b and pdb_id[:4]:
 					domain_boundary = pdb_id[14:].strip()
 
 					val, segmented = getCathDict(domain_boundary, domains)
-
-					if segmented==True:
-						continue
-
-					# print pdb_id
+					
 					visited_list.append(pdb_id[:4])
 
+
+					if segmented==True and c=='C':
+						continue
+
+					if segmented==False and c=='N':
+						continue
+
+					print pdb_id
 
 					cords_list, realId_list = getCordsList(var_1,chain)
 
@@ -463,77 +446,122 @@ for pdb_file in os.listdir(path):
 
 					simulated_domains = 1
 
-					while(simulated_domains!=3):
+					while(simulated_domains!=b+1):
 
 						km = KMeans(n_clusters=simulated_domains).fit(x)
-
 						labels_km = km.labels_
 
-						if simulated_domains==2:
-							length_list_2.append(len(cords_list))
-							gyration_list_2.append(radius_of_gyration.radius_of_gyration(var_2, chain))
-							interactionEnergy_list_2.append(getInteractionEnergy(labels_km, simulated_domains, cords_list))
+						if simulated_domains==b:
+							length_list_b.append(len(cords_list))
+							gyration_list_b.append(radius_of_gyration.radius_of_gyration(var_2, chain))
+							interactionEnergy_list_b.append(getInteractionEnergy(labels_km, simulated_domains, cords_list))
 
 						simulated_domains+=1	
 
 
-# print gyration_list[0]
-# single_domain_length = "single_domain_length"
-# single_domain_rg = "single_domain_rg"
-# single_domain_interaction_energy  = "single_domain_interaction_energy"
+domain_string_a=""
 
-# two_domain_length_non_contiguous = "two_domain_length_non_contiguous"
-# two_domain_rg_non_contiguous = "two_domain_rg_non_contiguous"
-# two_domain_interaction_energy_non_contiguous = "two_domain_interaction_energy_non_contiguous"
+if(a==1):
+	domain_string_a="single"
 
-two_domain_length_contiguous = "two_domain_length_contiguous"
-two_domain_rg_contiguous = "two_domain_rg_contiguous"
-two_domain_interaction_energy_contiguous = "two_domain_interaction_energy_contiguous"
+if(a==2):
+	domain_string_a="two"
+
+if(a==3):
+	domain_string_a="three"
+
+if(a==4):
+	domain_string_a="four"
+
+if(a==5):
+	domain_string_a="five"
 
 
-# fp1 = open(single_domain_length, 'wb')
-# pickle.dump(length_list, fp1)
-# fp1.close()
+length_a = domain_string_a + "_domain_length"
+rg_a = domain_string_a + "_domain_rg"
+interaction_energy_a  = domain_string_a + "_domain_interaction_energy"
 
-# fp2 = open(single_domain_rg, 'wb')
-# pickle.dump(gyration_list, fp2)
-# fp2.close()
+domain_string_b=""
 
-# fp3 = open(single_domain_interaction_energy, 'wb')
-# pickle.dump(interactionEnergy_list, fp3)
-# fp3.close()
+if(b==2):
+	domain_string_b="two"
+if(b==3):
+	domain_string_b="three"
 
-# fp4 = open(two_domain_length_contiguous, 'wb')
-# pickle.dump(length_list_2, fp4)
-# fp4.close()
+if(b==4):
+	domain_string_b="four"
 
-# fp5 = open(two_domain_rg_contiguous, 'wb')
-# pickle.dump(gyration_list_2, fp5)
-# fp5.close()
+if(b==5):
+	domain_string_b="five"
 
-# fp6 = open(two_domain_interaction_energy_contiguous, 'wb')
-# pickle.dump(interactionEnergy_list_2, fp6)
-# fp6.close()
+
+length_b_non_contiguous = domain_string_b + "_domain_length_non_contiguous"
+rg_b_non_contiguous = domain_string_b + "_domain_rg_non_contiguous"
+interaction_energy_b_non_contiguous =  domain_string_b + "_domain_interaction_energy_non_contiguous"
+
+length_b_contiguous = domain_string_b + "_domain_length_contiguous"
+rg_b_contiguous = domain_string_b + "_domain_rg_contiguous"
+interaction_energy_b_contiguous = domain_string_b + "_domain_interaction_energy_contiguous"
+
+
+fp1 = open(length_a, 'wb')
+pickle.dump(length_list_a, fp1)
+fp1.close()
+
+fp2 = open(rg_a, 'wb')
+pickle.dump(gyration_list_a, fp2)
+fp2.close()
+
+fp3 = open(interaction_energy_a, 'wb')
+pickle.dump(interactionEnergy_list_a, fp3)
+fp3.close()
+
+if c=='C':
+	fp4 = open(length_b_contiguous, 'wb')
+	pickle.dump(length_list_b, fp4)
+	fp4.close()
+
+	fp5 = open(rg_b_contiguous, 'wb')
+	pickle.dump(gyration_list_b, fp5)
+	fp5.close()
+
+	fp6 = open(interaction_energy_b_contiguous, 'wb')
+	pickle.dump(interactionEnergy_list_b, fp6)
+	fp6.close()
+
+if c=='N':
+	fp4 = open(length_b_non_contiguous, 'wb')
+	pickle.dump(length_list_b, fp4)
+	fp4.close()
+
+	fp5 = open(rg_b_non_contiguous, 'wb')
+	pickle.dump(gyration_list_b, fp5)
+	fp5.close()
+
+	fp6 = open(interaction_energy_b_non_contiguous, 'wb')
+	pickle.dump(interactionEnergy_list_b, fp6)
+	fp6.close()
+
 
 
 # print "Single Domain Proteins"
-# print len(gyration_list)
-# print len(interactionEnergy_list)
-# print len(length_list)
+print len(gyration_list_a)
+print len(interactionEnergy_list_a)
+print len(length_list_a)
 
 
 # print "Multi Domain Proteins"
-# print len(gyration_list_2)
-# print len(interactionEnergy_list_2)
-# print len(length_list_2)
+print len(gyration_list_b)
+print len(interactionEnergy_list_b)
+print len(length_list_b)
 
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
 # # ax1 = fig.add_subplot(111, projection='3d')
 
 
-# # ax.scatter(gyration_list, length_list, interactionEnergy_list, c='r')
-# ax.scatter(gyration_list_2, length_list_2, interactionEnergy_list_2, c='b')
+# # ax.scatter(gyration_list_a, length_list_a, interactionEnergy_list_a, c='r')
+# ax.scatter(gyration_list_b, length_list_b, interactionEnergy_list_b, c='b')
 
 # ax.set_xlabel('Radius Of Gyration')
 # ax.set_ylabel('Length')
@@ -542,34 +570,12 @@ two_domain_interaction_energy_contiguous = "two_domain_interaction_energy_contig
 # plt.show()
 
 
-# for i in range(0, max_length, 5):
-# 	j=0.0
-# 	while(j<=max_energy):
-# 		tp=0
-# 		fp=0		
-# 		for a in range(len(length_list_2)):
-# 			if length_list_2[a] >= i and float(interactionEnergy_list_2[a]) <= float(j):
-# 				tp+=1
+# plt.plot(length_list_a, interactionEnergy_list_a,'ro')
+# plt.plot(length_list_b, interactionEnergy_list_b, 'bo')
+# # plt.plot(length_list_b, y_line)
 
-# 		for a in range(len(length_list)):
-# 			if length_list[a] >=i and float(interactionEnergy_list[a]) <= float(j):
-# 				fp+=1
-
-
-# 		print "For length greater than " + str(i) + " And interacton energy < " + str(float(j)) + " True positives " + str(tp) + "/" + str(len(length_list_2)) + " False positives " + str(fp) + "/" + str(len(length_list))
-# 		j+=0.2
-
-
-
-
-
-
-# plt.plot(length_list, interactionEnergy_list,'ro')
-# plt.plot(length_list_2, interactionEnergy_list_2, 'bo')
-# # plt.plot(length_list_2, y_line)
-
-# # plt.plot(gyration_list, interactionEnergy_list,'ro')
-# # plt.plot(gyration_list_2, interactionEnergy_list_2, 'bo')
+# # plt.plot(gyration_list_a, interactionEnergy_list_a,'ro')
+# # plt.plot(gyration_list_b, interactionEnergy_list_b, 'bo')
 
 # plt.xlabel('Length')
 # # plt.xlabel('Radius Of Gyration')
@@ -581,23 +587,3 @@ two_domain_interaction_energy_contiguous = "two_domain_interaction_energy_contig
 # plt.show()
 
 
-# 					clusters_km = km.cluster_centers_
-
-# 					boundaries = domainBoundaries(labels_km, realId_list,domains)
-
-# 					boundaries = fillVoids(boundaries)
-# 					boundaries = stitchPatches(boundaries, clusters_km, cords_list, realId_list, 15)
-
-# 					cathDict = getCathDict(domain_boundary, domains)
-
-# 					cathDict, boundaries = mapCorrectly(cathDict, boundaries)
-
-# 					overlap = compareResults(cathDict, boundaries, domains)
-
-# 					accuracy = accuracy + overlap
-
-# 					for key, value in boundaries.iteritems():
-# 						makeReadable(value)
-# 					print ", " + "{0:.2f}".format(overlap)
-
-# print "accuracy is", accuracy/file_counter
--- Scripts/truePositives_falsePositives.py
+++ Scripts/truePositives_falsePositives.py
@@ -1,42 +1,104 @@
 import pickle
 import matplotlib.pyplot as plt
 
-fp1 = open('single_domain_length', 'rb')
-single_domain_length = pickle.load(fp1)
+a = int(raw_input("No. of domains(lesser) for protein A: "))
+b = int(raw_input("No. of domains(higher) for protein B: "))
+c = raw_input("Contiguous(C)/Non-Contiguous(N)/Both(B): ")
 
-fp2 = open('single_domain_rg', 'rb')
-single_domain_rg = pickle.load(fp2)
+domain_string_a=""
 
-fp3 = open('single_domain_interaction_energy', 'rb')
-single_domain_interaction_energy = pickle.load(fp3)
+if(a==1):
+	domain_string_a="single"
 
-fp4 = open('two_domain_length_contiguous', 'rb')
-two_domain_length_contiguous = pickle.load(fp4)
+if(a==2):
+	domain_string_a="two"
 
-fp5 = open('two_domain_rg_contiguous', 'rb')
-two_domain_rg_contiguous = pickle.load(fp5)
+if(a==3):
+	domain_string_a="three"
 
-fp6 = open('two_domain_interaction_energy_contiguous', 'rb')
-two_domain_interaction_energy_contiguous = pickle.load(fp6)
+if(a==4):
+	domain_string_a="four"
 
-fp7 = open('two_domain_length_non_contiguous', 'rb')
-two_domain_length_non_contiguous = pickle.load(fp7)
+if(a==5):
+	domain_string_a="five"
 
-fp8 = open('two_domain_rg_non_contiguous', 'rb')
-two_domain_rg_non_contiguous = pickle.load(fp8)
 
-fp9 = open('two_domain_interaction_energy_non_contiguous', 'rb')
-two_domain_interaction_energy_non_contiguous = pickle.load(fp9)
+length_a = domain_string_a + "_domain_length"
+rg_a = domain_string_a + "_domain_rg"
+interaction_energy_a  = domain_string_a + "_domain_interaction_energy"
+
+fp1 = open(length_a, 'rb')
+list_length_a = pickle.load(fp1)
+
+fp2 = open(rg_a, 'rb')
+list_rg_a = pickle.load(fp2)
+
+fp3 = open(interaction_energy_a, 'rb')
+list_interaction_energy_a = pickle.load(fp3)
+
+domain_string_b=""
+
+if(b==2):
+	domain_string_b="two"
+if(b==3):
+	domain_string_b="three"
+
+if(b==4):
+	domain_string_b="four"
+
+if(b==5):
+	domain_string_b="five"
+
+length_b_contiguous = domain_string_b + "_domain_length_contiguous"
+rg_b_contiguous = domain_string_b + "_domain_rg_contiguous"
+interaction_energy_b_contiguous = domain_string_b + "_domain_interaction_energy_contiguous"
+
+length_b_non_contiguous = domain_string_b + "_domain_length_non_contiguous"
+rg_b_non_contiguous = domain_string_b + "_domain_rg_non_contiguous"
+interaction_energy_b_non_contiguous =  domain_string_b + "_domain_interaction_energy_non_contiguous"
+
+
+
+
+fp4 = open(length_b_contiguous, 'rb')
+list_length_b_contiguous = pickle.load(fp4)
+
+fp5 = open(rg_b_contiguous, 'rb')
+list_rg_b_contiguous = pickle.load(fp5)
+
+fp6 = open(interaction_energy_b_contiguous, 'rb')
+list_interaction_energy_b_contiguous = pickle.load(fp6)
+
+fp7 = open(length_b_non_contiguous, 'rb')
+list_length_b_non_contiguous = pickle.load(fp7)
+
+fp8 = open(rg_b_non_contiguous, 'rb')
+list_rg_b_non_contiguous = pickle.load(fp8)
+
+fp9 = open(interaction_energy_b_non_contiguous, 'rb')
+list_interaction_energy_b_non_contiguous = pickle.load(fp9)
 
 val = -500000
 
-max_rg = max(max(single_domain_rg), max(two_domain_rg_contiguous) ,max(two_domain_rg_non_contiguous))
-max_length = max(max(single_domain_length), max(two_domain_length_contiguous), max(two_domain_length_non_contiguous))
-max_energy = max(max(single_domain_interaction_energy), max(two_domain_interaction_energy_contiguous), max(two_domain_interaction_energy_non_contiguous))
+max_rg = max(max(list_rg_a), max(list_rg_b_non_contiguous), max(list_rg_b_contiguous))
+max_length = max(max(list_length_a), max(list_length_b_contiguous), max(list_length_b_non_contiguous))
+max_energy = max(max(list_interaction_energy_a), max(list_interaction_energy_b_contiguous), max(list_interaction_energy_b_non_contiguous))
 
-list_length = two_domain_length_contiguous
-list_rg = two_domain_rg_contiguous
-list_interaction_energy = two_domain_interaction_energy_contiguous
+if c=='C':
+	list_length_b = list_length_b_contiguous
+	list_rg_b = list_rg_b_contiguous
+	list_interaction_energy_b = list_interaction_energy_b_contiguous
+
+if c=='N':
+	list_length_b = list_length_b_non_contiguous
+	list_rg_b = list_rg_b_non_contiguous
+	list_interaction_energy_b = list_interaction_energy_b_non_contiguous
+
+if c=='B':
+	list_length_b = list_length_b_non_contiguous + list_length_b_contiguous
+	list_rg_b = list_rg_b_non_contiguous + list_rg_b_contiguous
+	list_interaction_energy_b = list_interaction_energy_b_non_contiguous + list_interaction_energy_b_contiguous
+
 
 ans = [0,0,0,0]
 
@@ -45,12 +107,13 @@ for i in range(0, max_length, 5):
 	while(j <= max_energy):
 		fp=0		
 		tp=0
-		for a in range(len(list_length)):
-			if int(list_length[a]) >= i and float(list_interaction_energy[a]) <= j:
+		for k in range(len(list_length_b)):
+			if int(list_length_b[k]) >= i and float(list_interaction_energy_b[k]) <= j:
 				tp+=1
 
-		for a in range(len(single_domain_length)):
-			if int(single_domain_length[a]) >=i and float(single_domain_interaction_energy[a]) <= j:
+		for k in range(len(list_length_a)):
+			if int(list_length_a[k]) >=i and float(list_interaction_energy_a[k]) <= j:
+				# print list_length_a[k], list_interaction_energy_a[k]
 				fp+=1
 
 		if tp-fp >= val:
@@ -62,7 +125,7 @@ for i in range(0, max_length, 5):
 
 		j = j + 0.02
 
-print "For length >= " + str(ans[0]) + " And interacton energy <= " + str(ans[1]) + " True positives " + str(ans[2]) + "/" + str(len(list_length)) + " False positives " + str(ans[3]) + "/" + str(len(single_domain_length))
+print "For length >= " + str(ans[0]) + " And interacton energy <= " + str(ans[1]) + " True positives " + str(ans[2]) + "/" + str(len(list_length_b)) + " False positives " + str(ans[3]) + "/" + str(len(list_length_a))
 
 
 val = -500000
@@ -75,14 +138,14 @@ for i in range(0, int(max_rg), 2):
 		fp=0		
 		tp=0
 		# print "initial tp and fp ", tp, fp
-		for a in range(len(list_rg)):
-			if float(list_rg[a]) >= i and float(list_interaction_energy[a]) <= j:
+		for k in range(len(list_rg_b)):
+			if float(list_rg_b[k]) >= i and float(list_interaction_energy_b[k]) <= j:
 				# print two_domain_rg[a], two_domain_interaction_energy[a], j
 				tp+=1
 
-		for a in range(len(single_domain_rg)):
-			if float(single_domain_rg[a]) >= i and float(single_domain_interaction_energy[a]) <= j:
-				# print single_domain_rg[a], single_domain_interaction_energy[a], j
+		for k in range(len(list_rg_a)):
+			if float(list_rg_a[k]) >= i and float(list_interaction_energy_a[k]) <= j:
+				# print rg_a[a], interaction_energy_a[a], j
 				fp+=1
 
 		
@@ -97,7 +160,7 @@ for i in range(0, int(max_rg), 2):
 		j = j + 0.02
 
 
-print "For rg >= " + str(ans[0]) + " And interacton energy <= " + str(float(ans[1])) + " True positives " + str(ans[2]) + "/" + str(len(list_length)) + " False positives " + str(ans[3]) + "/" + str(len(single_domain_length))
+print "For rg >= " + str(ans[0]) + " And interacton energy <= " + str(float(ans[1])) + " True positives " + str(ans[2]) + "/" + str(len(list_rg_b)) + " False positives " + str(ans[3]) + "/" + str(len(list_rg_a))
 
 
 val = -500000
@@ -113,12 +176,12 @@ for i in range(0, max_length, 5):
 		while(k <= max_energy):
 			fp=0		
 			tp=0
-			for a in range(len(list_rg)):
-				if list_length[a] >= i and float(list_rg[a]) >= j and float(list_interaction_energy[a]) <= k:
+			for l in range(len(list_length_b)):
+				if list_length_b[l] >= i and float(list_rg_b[l]) >= j and float(list_interaction_energy_b[l]) <= k:
 					tp+=1
 
-			for a in range(len(single_domain_rg)):
-				if single_domain_length[a] >= i and float(single_domain_rg[a]) >= j and float(single_domain_interaction_energy[a]) <= k:
+			for m in range(len(list_rg_a)):
+				if list_length_a[m] >= i and float(list_rg_a[m]) >= j and float(list_interaction_energy_a[m]) <= k:
 					fp+=1
 
 			# print tp, fp
@@ -135,77 +198,69 @@ for i in range(0, max_length, 5):
 			k = k + 0.02
 
 
-print "For length >= " + str(ans[0]) + " And Rg >= " + str(float(ans[1]))+ " And interacton energy <= " + str(float(ans[2])) + " True positives " + str(ans[3]) + "/" + str(len(list_length)) + " False positives " + str(ans[4]) + "/" + str(len(single_domain_length))
+print "For length >= " + str(ans[0]) + " And Rg >= " + str(float(ans[1]))+ " And interacton energy <= " + str(float(ans[2])) + " True positives " + str(ans[3]) + "/" + str(len(list_length_b)) + " False positives " + str(ans[4]) + "/" + str(len(list_length_a))
 
 y_line_interaction_energy = [ans[2] for x in range(7)]
 x_line_length = [ans[0] for x in range(7)]
 x_line_rg  = [ans[1] for x in range(7)]
 
-for x in range(len(single_domain_length)):
-	if single_domain_length[x] >= ans[0] and single_domain_interaction_energy[x] <= ans[2]:
-		print single_domain_length[x], single_domain_interaction_energy[x]
 
 
-# plt.plot(single_domain_length, single_domain_interaction_energy,'ro')
-# plt.plot(two_domain_length_contiguous, two_domain_interaction_energy_contiguous, 'bo')
+# plt.plot(list_length_a, list_interaction_energy_a,'ro')
+# plt.plot(list_length_b_contiguous, list_interaction_energy_b_contiguous, 'bo')
+# plt.xlabel('Length')
+# plt.ylabel('Interaction Energy')
+# plt.show()
+
+
+
+# plt.plot(list_rg_a, list_interaction_energy_a, 'ro')
+# plt.plot(list_rg_b_contiguous, list_interaction_energy_b_contiguous, 'bo')
+# plt.plot([10,15,20,25,30,35,40], y_line_interaction_energy)
+# plt.plot(x_line_rg, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
+# plt.xlabel('Radius of Gyration')
+# plt.ylabel('Interaction Energy')
+# plt.show()
+
+
+
+
+# plt.plot(list_length_a, list_interaction_energy_a,'ro')
+# plt.plot(list_length_b_non_contiguous, list_interaction_energy_b_non_contiguous, 'go')
 # plt.plot([0,100,200,300,400,500,600], y_line_interaction_energy)
 # plt.plot(x_line_length, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
 # plt.xlabel('Length')
 # plt.ylabel('Interaction Energy')
 # plt.show()
 
-# plt.plot(single_domain_rg, single_domain_interaction_energy, 'ro')
-# plt.plot(two_domain_rg_contiguous, two_domain_interaction_energy_contiguous, 'bo')
+
+
+# plt.plot(list_rg_a, list_interaction_energy_a, 'ro')
+# plt.plot(list_rg_b_non_contiguous, list_interaction_energy_b_non_contiguous, 'go')
 # plt.plot([10,15,20,25,30,35,40], y_line_interaction_energy)
 # plt.plot(x_line_rg, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
 # plt.xlabel('Radius of Gyration')
-# plt.ylabel('Interaction Energy')
-
-
-# plt.plot(single_domain_length, single_domain_interaction_energy,'ro')
-# plt.plot(two_domain_length_non_contiguous, two_domain_interaction_energy_non_contiguous, 'go')
-# plt.plot([0,100,200,300,400,500,600], y_line_interaction_energy)
-# plt.plot(x_line_length, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
-# plt.xlabel('Length')
-# plt.ylabel('Interaction Energy')
-
-# plt.plot(single_domain_rg, single_domain_interaction_energy, 'ro')
-# plt.plot(two_domain_rg_non_contiguous, two_domain_interaction_energy_non_contiguous, 'go')
-# plt.plot([10,15,20,25,30,35,40], y_line_interaction_energy)
-# plt.plot(x_line_rg, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
-# plt.xlabel('Radius of Gyration')
-# plt.ylabel('Interaction Energy')
-
-# plt.plot(single_domain_length, single_domain_interaction_energy,'ro')
-# plt.plot(two_domain_length_contiguous, two_domain_interaction_energy_contiguous, 'bo')
-# plt.plot(two_domain_length_non_contiguous, two_domain_interaction_energy_non_contiguous, 'go')
-# plt.plot([0,100,200,300,400,500,600], y_line_interaction_energy)
-# plt.plot(x_line_length, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
-# plt.xlabel('Length')
 # plt.ylabel('Interaction Energy')
 # plt.show()
 
-plt.plot(single_domain_rg, single_domain_interaction_energy, 'ro')
-plt.plot(two_domain_rg_contiguous, two_domain_interaction_energy_contiguous, 'bo')
-plt.plot(two_domain_rg_non_contiguous, two_domain_interaction_energy_non_contiguous, 'go')
-plt.plot([10,15,20,25,30,35,40], y_line_interaction_energy)
-plt.plot(x_line_rg, [0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12])
+
+
+plt.plot(list_length_a, list_interaction_energy_a,'ro')
+plt.plot(list_length_b_contiguous, list_interaction_energy_b_contiguous, 'bo')
+plt.plot(list_length_b_non_contiguous, list_interaction_energy_b_non_contiguous, 'go')
+plt.plot([0,400,600,800,1200,1400,1600], y_line_interaction_energy)
+plt.plot(x_line_length, [0.0, 0.02, 0.04, 0.06, 0.08, 0.14, 0.20])
+plt.xlabel('Length')
+plt.ylabel('Interaction Energy')
+plt.show()
+
+
+
+plt.plot(list_rg_a, list_interaction_energy_a, 'ro')
+plt.plot(list_rg_b_contiguous, list_interaction_energy_b_contiguous, 'bo')
+plt.plot(list_rg_b_non_contiguous, list_interaction_energy_b_non_contiguous, 'go')
+plt.plot([10,15,20,25,30,40,50], y_line_interaction_energy)
+plt.plot(x_line_rg, [0.0, 0.02, 0.04, 0.06, 0.08, 0.14, 0.20])
 plt.xlabel('Radius of Gyration')
 plt.ylabel('Interaction Energy')
-
-
-
-
-# plt.plot(single_domain_length, single_domain_interaction_energy,'ro')
-# plt.plot(two_domain_length_contiguous, two_domain_interaction_energy_contiguous, 'bo')
-# plt.plot(two_domain_length_non_contiguous, two_domain_interaction_energy_non_contiguous, 'go')
-
-# plt.plot(single_domain_rg, single_domain_interaction_energy,'ro')
-# plt.plot(two_domain_rg_contiguous, two_domain_interaction_energy_contiguous, 'bo')
-# plt.plot(two_domain_rg_non_contiguous, two_domain_interaction_energy_non_contiguous, 'go')
-
-
-# plt.xlabel('length')
-# plt.ylabel('Interaction Energy')
-
 plt.show()
