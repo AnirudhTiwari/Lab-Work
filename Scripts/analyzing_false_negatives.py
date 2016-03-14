115c28ced695098a8baa5d372d3879aed00b7afa
Anirudh Tiwari <tiwariisonline@gmail.com>
2016-03-15 01:58:16 +0530

    Analyzed false negatives for contigous and non contigous nature

---
+++ Scripts/analyzing_false_negatives.py
@@ -0,0 +1,46 @@
+def isContiguous(cath_boundaries, domains):
+	# print cath_boundaries
+	cath_boundaries = cath_boundaries.split(" ")
+	cath_boundaries = filter(None, cath_boundaries)
+	cathDict = {}
+	key = 0
+	numOFSegments = 1
+	x = 0
+	while 1:
+		if x >= len(cath_boundaries):
+			break
+		else:
+			numOFSegments = int(cath_boundaries[x])
+			if numOFSegments > 1:
+				return False
+			dom = cath_boundaries[x:x+6*numOFSegments+1]
+			cathDict[key] = dom
+			key+=1
+			x+=6*numOFSegments+1
+	return True
+
+with open('false_negatives_entries', 'r') as f1:
+	neg_entries = f1.readlines()
+
+
+with open('../Input Files/CathDomall', 'r') as f2:
+	cath_entries = f2.readlines()
+
+
+for x in neg_entries:
+	neg_pdb = x[:4].strip()
+	neg_chain = x[4].lower().strip()
+
+	for y in cath_entries:
+		if y[0]!='#':
+			cath_pdb = y[:4].lower().strip()
+
+			cath_chain = y[18].lower().strip()
+
+			cath_domains = int(y[7] + y[8])
+
+			if cath_pdb==neg_pdb and cath_chain==neg_chain:
+				print y, isContiguous(y[15:], cath_domains)
+				print
+
+
